9769124ea7be7c02c5777a6a88e70287a578636a
Cheng <ccchiu@umich.edu>
2024-04-15 16:34:51 -0400

    pipeline ok

---
+++ index_server/index/api/main.py
@@ -0,0 +1,12 @@
+"""Following."""
+import flask
+from flask import request, session, abort, url_for, redirect
+
+
+@.route('/api/v1/')
+def index(user_url_slug):
+    return 0
+
+@.route('/api/v1/hits/')
+def index(user_url_slug):
+    return 0
